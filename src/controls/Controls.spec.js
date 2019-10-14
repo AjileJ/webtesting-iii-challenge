0bfff3fb7e476b89e2f5610e80a89f417a07f330
AjileJ <53097022+AjileJ@users.noreply.github.com>
2019-10-14 16:01:07 -0700

    did the best i could

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,26 @@
 // Test away!
+import React from 'react';
+import {render} from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import 'react-testing-library/cleanup-after-each';
+import Controls from './Controls';
+
+
+test('it renders correctly', ()=> {
+  expect(render(<Controls />)).toMatchSnapshot();
+});
+
+describe('<Controls />', () => {
+  it('button is disabled', ()=> {
+    const {getByText, getByTestId,container} = render(
+    <Controls locked={true} closed={true}/>
+    );
+    const disabledButton = getByText(/disabled/i);
+
+    expect(getByTestId(container, disabledButton)).toBeDisabled();
+    expect(disabledButton).toHaveAttribute('disabled');
+  });
+  it('unmounts component after each test', () => {
+    console.log(document.body.outerHTML);
+  });
+});
